911fbdde1cdaf1a6c267a04dd40184e0797c6831
Willian de Almeida Oliveira <wlln.almeida.oliveira@gmail.com>
2024-01-25 14:54:09 -0300

    UserLoginDAO Func para descobrir o tipo de User.

--- WMJC/src/main/java/br/com/wmjc/db/user/UserLoginDAO.java
+++ WMJC/src/main/java/br/com/wmjc/db/user/UserLoginDAO.java
@@ -1,17 +1,17 @@
 package br.com.wmjc.db.user;
 
 import br.com.wmjc.config.ConnectionPoolConfig;
+import br.com.wmjc.model.user.UserModel;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-public class UserLoginDAO
-{
-    public boolean verificarLogin(String email, String senha)
-    {
-        try (Connection connection = ConnectionPoolConfig.getConnection())
-        {
+public class UserLoginDAO {
+    public boolean verificarLogin(String email, String senha) {
+        try (Connection connection = ConnectionPoolConfig.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM use WHERE email = ? AND senha = ?");
             preparedStatement.setString(1, email);
             preparedStatement.setString(2, senha);
@@ -21,16 +21,39 @@ public class UserLoginDAO
                 return true;
             }
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Erro ao verificar login: " + e.getMessage());
         }
         return false;
     }
 
-    public boolean verificarCadastro()
-    {
-        return false;
+    public List<UserModel> UsertypeFinder(String email, String senha) {
+        try (Connection connection = ConnectionPoolConfig.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM use WHERE email = ? AND senha = ?");
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, senha);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<UserModel> users = new ArrayList<>();
+
+            while (resultSet.next()) {
+                String mail = resultSet.getString("email");
+                String sen = resultSet.getString("senha");
+                String tipoUSer = resultSet.getString("tipoDeUsario");
+
+                UserModel user = new UserModel(mail, sen, tipoUSer);
+                users.add(user);
+            }
+
+            System.out.println("Lista de usuarios: " + users);
+            connection.close();
+            return users;
+
+        } catch (Exception e) {
+            System.out.println("Erro ao verificar login: " + e.getMessage());
+
+            return null;
+        }
     }
 }
+
