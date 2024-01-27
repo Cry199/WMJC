b43c3f68928e2ff9c255e48b0f987331cf13428f
Willian de Almeida Oliveira <wlln.almeida.oliveira@gmail.com>
2024-01-27 18:17:22 -0300

    Criando a DAO do Comentário do Perfil.

---
+++ WMJC/src/main/java/br/com/wmjc/db/user/ProfileCommentDAO.java
@@ -0,0 +1,47 @@
+package br.com.wmjc.db.user;
+
+import br.com.wmjc.config.ConnectionPoolConfig;
+import br.com.wmjc.model.user.profile.Comments.ProfileComments;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileCommentDAO {
+
+    public List<ProfileComments> CommentList(Integer idPerfil) {
+
+        try (Connection connection = ConnectionPoolConfig.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM ComentariosPerfil Where iddoUser = ?");
+            preparedStatement.setInt(1, new Integer(idPerfil));
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<ProfileComments> comments = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("iddoUser");
+                String comment = resultSet.getString("Comentario");
+                Integer perfil = resultSet.getInt("idPerfil");
+
+                ProfileComments profileComments = new ProfileComments(id,comment, perfil);
+                comments.add(profileComments);
+            }
+
+            System.out.println("Lista de comentarios: " + comments);
+            connection.close();
+            return comments;
+
+        } catch (Exception e) {
+            System.out.println("Nenhum comentário no perfil: " + e.getMessage());
+
+            return null;
+        }
+    }
+
+
+}
