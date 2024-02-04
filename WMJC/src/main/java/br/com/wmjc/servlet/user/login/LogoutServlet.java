667b01c80f857eaa9a377c14e181c07a0456b5e1
Cauã <caualeu2016@gmail.com>
2024-02-04 06:04:35 -0300

    Create: LogoutServlet

---
+++ WMJC/src/main/java/br/com/wmjc/servlet/user/login/LogoutServlet.java
@@ -0,0 +1,22 @@
+package br.com.wmjc.servlet.user.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        req.getSession().invalidate();
+
+        req.setAttribute("message", "Usuario deslogado com sucesso!");
+
+        req.getRequestDispatcher("/Pages/jsp/login/login.jsp").forward(req, resp);
+    }
+}
