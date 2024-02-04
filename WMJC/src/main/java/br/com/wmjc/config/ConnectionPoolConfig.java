702d8a22ad91d88acb784acc8d4a1deb0cc2d975
Cauã <caualeu2016@gmail.com>
2024-02-04 05:58:32 -0300

    Uptade: ConnectionPoolConfig.java

--- WMJC/src/main/java/br/com/wmjc/config/ConnectionPoolConfig.java
+++ WMJC/src/main/java/br/com/wmjc/config/ConnectionPoolConfig.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectionPoolConfig {
 
@@ -15,9 +16,10 @@ public class ConnectionPoolConfig {
 
     public static BasicDataSource getDataSource() {
 
-        if (dataSource == null) {
+        if (dataSource == null)
+        {
             dataSource = new BasicDataSource();
-            dataSource.setUrl("jdbc:h2:~/test");
+            dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_ON_EXIT=FALSE");
             dataSource.setUsername("sa");
             dataSource.setPassword("sa");
             dataSource.setMinIdle(5);
@@ -25,15 +27,38 @@ public class ConnectionPoolConfig {
             dataSource.setMaxTotal(50);
 
             System.out.println("New connection pool created with successful");
-
         }
 
         return dataSource;
     }
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException { return getDataSource().getConnection(); }
 
-        return getDataSource().getConnection();
+    public static void backupDatabase()
+    {
+        try (Connection conn = getDataSource().getConnection(); Statement stmt = conn.createStatement())
+        {
+            System.out.println("Fazendo backup do banco de dados");
+            stmt.execute("SCRIPT TO 'src/main/java/br/com/wmjc/db/script/db.sql'");
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Erro ao fazer backup");
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public static void restoreDatabase()
+    {
+        try (Connection conn = getDataSource().getConnection(); Statement stmt = conn.createStatement())
+        {
+            System.out.println("Rodando script");
+            stmt.execute("RUNSCRIPT FROM 'src/main/java/br/com/wmjc/db/script/db.sql'");
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Erro ao restaurar backup");
+            System.out.println(e.getMessage());
+        }
     }
 }
