a1d103cf90001a454a0a51aed4b72ea0c7b51b4f
Cauã <caualeu2016@gmail.com>
2024-02-09 19:40:24 -0300

    Create: GameInfoEditDeleteServlet.java

---
+++ WMJC/src/main/java/br/com/wmjc/servlet/game/GameInfoEditDeleteServlet.java
@@ -0,0 +1,26 @@
+package br.com.wmjc.servlet.game;
+
+import br.com.wmjc.db.game.GameDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/gameInfoEditDelete")
+public class GameInfoEditDeleteServlet extends HttpServlet
+{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        String id = req.getParameter("id");
+
+        new GameDAO().deleteGame(id);
+
+        req.setAttribute("message", "Deleted with success!");
+
+        resp.sendRedirect("/Jogos");
+    }
+}
