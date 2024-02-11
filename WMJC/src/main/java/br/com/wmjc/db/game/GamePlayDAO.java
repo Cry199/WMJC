ca013974c6016b57924ca2d7261461cb645389b7
Cauã <caualeu2016@gmail.com>
2024-02-11 04:32:50 -0300

    Create: GamePlayDAO.java

---
+++ WMJC/src/main/java/br/com/wmjc/db/game/GamePlayDAO.java
@@ -0,0 +1,66 @@
+package br.com.wmjc.db.game;
+
+import br.com.wmjc.config.ConnectionPoolConfig;
+import br.com.wmjc.model.game.GameGeneralModel;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GamePlayDAO
+{
+    public GameGeneralModel buscandoGamePorId(String tabelaGame,String id)
+    {
+        String sql = "SELECT * FROM " + tabelaGame +" WHERE id = ?";
+
+        try(Connection connection = ConnectionPoolConfig.getConnection())
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            GameGeneralModel gameGeneralModel = new GameGeneralModel();
+
+            while (resultSet.next())
+            {
+                gameGeneralModel.setId(preparedStatement.getResultSet().getInt("id"));
+                gameGeneralModel.setTexto(preparedStatement.getResultSet().getString("texto"));
+                gameGeneralModel.setOp1(preparedStatement.getResultSet().getString("op1"));
+                gameGeneralModel.setRef1(preparedStatement.getResultSet().getString("ref1"));
+                gameGeneralModel.setOp2(preparedStatement.getResultSet().getString("op2"));
+                gameGeneralModel.setRef2(preparedStatement.getResultSet().getString("ref2"));
+                gameGeneralModel.setOp3(preparedStatement.getResultSet().getString("op3"));
+                gameGeneralModel.setRef3(preparedStatement.getResultSet().getString("ref3"));
+                gameGeneralModel.setImage(preparedStatement.getResultSet().getString("image"));
+            }
+
+            connection.close();
+
+            return gameGeneralModel;
+        }
+        catch (Exception e)
+        {
+            System.out.println("GamePlayDAO - Erro ao buscar game por id: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public boolean isTableExists(String nameTable)
+    {
+        String sql = "SELECT * FROM " + nameTable;
+
+        try (Connection connection = ConnectionPoolConfig.getConnection())
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeQuery();
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+}
